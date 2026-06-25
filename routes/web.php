c0c845a8ba05e7d112e1e38f04e417bba6b85a1e
Tur-nar <timmyturnerrr50@gmail.com>
2026-06-26 00:50:47 +0100

    feat: phase-4: infinite canvas

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
-use App\Http\Controllers\NoteController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [WelcomeController::class, 'welcome'])->name('welcome');
-
-Route::resource('notes', NoteController::class)->names('notes');
